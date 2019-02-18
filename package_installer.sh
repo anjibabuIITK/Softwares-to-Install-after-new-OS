8186a63419a19febaa370a9bfb6fb0f6ad6a813f
KAPAKAYALA ANJI BABU <anjibabu480@gmail.com>
2019-02-18 17:53:40 +0800

    uploaded by anji babu

---
+++ package_installer.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# 
+# Bash script to install required packages or tools after installation of new Operating System.
+#
+#  Authour: Anji Babu Kapakyala
+#           Dept. of Chemistry
+#           C/O Prof. Nisanth N. Nair
+#           IIT Kanpur, India.
+#
+#
+#  USAGE:  
+#   
+# 
+#  Packages List: 
+#		VMD
+#		CHIMERA
+#               Paymol
+#   		molden
+#		WPS Office
+#		xfig
+#		gv
+#		tree
+#		gnuplot
+#		Autodoc
+#		grace
+#		Anaconda 
+#		Imp Libraries [ sudo apt-get install csh flex patch gfortran g++ make xorg-dev libbz2-dev zlib1g-dev libboost-dev libboost-thread-dev libboost-system-dev ]
+#         	Amber
+#		Gromacs
+#		Plumed
+#		ParmEd
+#		LateX
+# 		SSH server
+#		gwenview
+#		mendely desktop
+#		gimp
+#		Teamviewer
+#		sublimetext3 ot Atom
+#=======================================================================#
+#  Defining Colors
+red=$(tput setaf 1)
+grn=$(tput setaf 2)
+ylw=$(tput setaf 3)
+blu=$(tput setaf 4)
+pur=$(tput setaf 5)
+cyn=$(tput setaf 6)
+reset=$(tput sgr0)
+bold=$(tput bold)
+#-----------------------------------------------------------------------#
+#  How to download package like WPS? from webpage in bash? findout 
+#
+#
+# 
+
+
+
+
